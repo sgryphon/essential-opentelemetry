f8de1687c9f4a7cb286a71ab8cc9336718360069
Sly Gryphon <sgryphon@computer.org>
2026-02-07 14:03:00 +1000

    Add trace to simple example

--- examples/SimpleConsole/Program.cs
+++ examples/SimpleConsole/Program.cs
@@ -1,8 +1,13 @@
 ﻿// Simple OpenTelemetry example
+using System.Diagnostics;
 using Essential.OpenTelemetry;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using OpenTelemetry.Trace;
+
+const string ServiceName = "SimpleConsole";
+var activitySource = new ActivitySource(ServiceName);
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Logging.ClearProviders();
@@ -11,8 +16,21 @@ builder
     .WithLogging(logging =>
     {
         logging.AddColoredConsoleExporter();
+    })
+    .WithTracing(tracing =>
+    {
+        tracing.AddSource(ServiceName).AddColoredConsoleExporter();
     });
 var host = builder.Build();
 
 var logger = host.Services.GetRequiredService<ILogger<Program>>();
-logger.LogInformation("Hello world");
+var tracerProvider = host.Services.GetRequiredService<TracerProvider>();
+
+// Create a simple activity (span)
+using (var activity = activitySource.StartActivity("SampleOperation"))
+{
+    logger.LogInformation("Hello world");
+}
+
+// Force flush to ensure activity is exported before exit
+tracerProvider.ForceFlush();
